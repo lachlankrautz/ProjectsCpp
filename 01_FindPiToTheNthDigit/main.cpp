881cbc0505da8151cae7ac66745f508fe6734f5a
lachlan.krautz <lachlan.krautz@gmail.com>
2015-10-03 14:08:19 +1000

    pi

---
+++ 01_FindPiToTheNthDigit/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+double getSqrt(double num);
+void loop();
+double getPiToDigit(int digit);
+
+int main()
+{
+    cout << "..::Pi Calculator::.." << endl;
+    cout << "Pi: " << getPiToDigit(10) << endl;
+    return 0;
+}
+
+void loop()
+{
+    bool quit = false;
+    while (!quit)
+    {
+
+    }
+}
+
+double getPiToDigit(int digit)
+{
+
+
+
+}
+
+double getPiDigit(int digit)
+{
+
+}
+
+double getSqrt(double num)
+{
+
+}
