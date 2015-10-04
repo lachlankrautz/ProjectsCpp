fd9efbd68adccb310cadfd2fc44da0ad6b6575aa
lachlan.krautz <lachlan.krautz@gmail.com>
2015-10-04 18:54:51 +1000

    number range message

--- 01_FindPiToTheNthDigit/main.cpp
+++ 01_FindPiToTheNthDigit/main.cpp
@@ -1,29 +1,55 @@
 #include <iostream>
 #include <sstream>
 #include <c++/iomanip>
+#include <c++/limits>
 
 using namespace std;
 
-void printSavedPi(int decimals);
-void printPi(int digit);
+int    getDecimals();
+void   printSavedPi(int decimals);
+void   printPi(int digit);
 double getPower(double num, int power);
 double getPiDigit(int digit);
 double getPiToDigit(int digit);
 
 int main()
 {
-    int decimals = 12;
-    cout << "..::Pi Calculator::.." << endl;
+    cout << " -[ Pi Calculator ]-" << endl;
+    int decimals = getDecimals();
     printSavedPi(decimals);
     printPi(decimals);
     return 0;
 }
 
+int getDecimals()
+{
+    int x = 0;
+    bool loop=true;
+    while(loop)
+    {
+        std::cout << "How many decimals? ";
+        std::string s;
+        std::getline(std::cin, s);
+
+        std::stringstream stream{s};
+        if(stream >> x && x > 0 && x < 12)
+        {
+            loop = false;
+        }
+        else
+        {
+            x = 0;
+            std::cout << "Invalid! Enter a number between 1 and 11" << std::endl;
+        }
+    }
+    return x;
+}
+
 void printSavedPi(int decimals)
 {
     std::string pi = "3.141592653589793238462643383279502884197169399375105820974944592307816406286";
-    cout << "Saved Pi: " << pi << endl;
-    cout << "Ans   Pi: " << pi.substr(0, (uint32_t)decimals+2) << endl;
+    //cout << "Saved Pi: " << pi << endl;
+    //cout << "Ans   Pi: " << pi.substr(0, (uint32_t)decimals+2) << endl;
 }
 
 void printPi(int decimals)
@@ -37,7 +63,7 @@ void printPi(int decimals)
     std::ostringstream pi;
     pi.precision(decimals+1);
     pi << rawPi;
-    cout << "Raw   Pi: " << pi.str() << endl;
+    //cout << "Raw   Pi: " << pi.str() << endl;
     cout << std::fixed;
     cout << std::setprecision(decimals);
     cout << "Gen   Pi: " << rawPi << endl;
